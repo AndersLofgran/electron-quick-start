9e40e5b3a598214b59db3467a2fe562126beaefb
Xadr123 <hansenxander@gmail.com>
2020-03-25 15:38:21 -0600

    final push

--- main.js
+++ main.js
@@ -10,8 +10,8 @@ require('electron-reload')(__dirname, {
 function createWindow () {
   // Create the browser window.
   const mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
+    width: 1280,
+    height: 720,
     // icon: '',
     webPreferences: {
       preload: path.join(__dirname, 'preload.js')
